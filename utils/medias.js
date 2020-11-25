8983f83a0dacc006b12c74129ab860d5f19d919a
Lissandre <lissandre.pasdeloup@hetic.net>
2020-11-25 20:05:08 +0100

    update env

--- utils/medias.js
+++ utils/medias.js
@@ -2,7 +2,7 @@ export function getStrapiMedia(url) {
   // Check if URL is a local path
   if (url.startsWith('/')) {
     // Prepend Strapi address
-    return `${process.env.strapiBaseUri}${url}`
+    return `https://back.tatchi.fr${url}`
   }
   // Otherwise return full URL
   return url
